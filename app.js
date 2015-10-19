c9502afb5e245ec0a723e2a8fb23cf35a6ee2ed8
grantb4812 <grantb4812@gmail.com>
2015-10-19 18:13:02 -0500

    finished random quote

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
  $('.button').on('click', function(){
-   var quote = $.ajax({
+   $.ajax({
      url: "http://api.icndb.com/jokes/random/"
    }).done(function(joke) {
-       $('.hide-show').hide
-       console.log(joke.value.joke);
+       var random = joke.value.joke;
+       var noSpace = random.replace(/ /g, '%20');
+     	$('.share').attr('href', "https://twitter.com/share?text="+noSpace+"");
+      	console.log(noSpace);
+       insertQuote(random);
    });
    
  });
 });
+
+var insertQuote = function (quote) {
+    $('.replace').replaceWith('<p class="replace">'+quote+'</p>');
+    $('.remove').remove();
+    $('.share').show();
+};
+
